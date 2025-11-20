4546fee6b0e58447fa51ebdc74a48c2d2e418c0b
vkhao <vkhao@Xubuntu.myguest.virtualbox.org>
2025-11-20 22:52:22 +0100

    v0.1.0
    Changelog:
    
    Made a working 'New' button:
    -Creates a new grid;
    -Gets rid of the old one;
    
    Improved part of the code;

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+const gridContainer = document.getElementById('gridContainer')
 const containerWidth = getComputedStyle(document.getElementById('gridContainer'), null).width
 const containerHeight = getComputedStyle(document.getElementById('gridContainer'), null).height
+
+const grid = document.querySelectorAll('#grid')
+const button = document.querySelectorAll('#button')
 
 function createDiv(num) {
     for (let i = 0; i<(num**2); i++) {
@@ -9,23 +13,34 @@ function createDiv(num) {
         div.style.height =  (parseInt(containerHeight)-6)/num + 'px'
         div.style.border = ('1px, solid, rgb(196, 211, 224)');
         div.id = 'grid'
+        div.addEventListener('mouseenter', mouseHover)
+        div.addEventListener('mouseout', mouseHover)
         gridContainer.appendChild(div)
     }
 }
 
 function mouseHover(event) {
     const target = event.currentTarget.classList.toggle('hover');
-    console.log(`The mouse is hovering ${target}`)
 }
 
-createDiv(4)
+function newBtn(event) {
+    const target = event.target.id.toLowerCase();
+    if (target === 'new') {
+        while (true) {
+            number = parseInt(prompt('Insert the grid\'s side size'))
+            if (number <= 64 && number >= 1) {
+                gridContainer.textContent = ''
+                createDiv(number)
+                break
+            }
+            else if (number > 64) {
+                alert('Number\'s too big! (max is 64). Try Again.')
+                continue
+            }
+        }
+    }
+}
 
-const grid = document.querySelectorAll('#grid')
-
-grid.forEach(pressed => {
-    pressed.addEventListener('mouseenter', mouseHover)
-})
-
-grid.forEach(pressed => {
-    pressed.addEventListener('mouseout', mouseHover)
+button.forEach(pressed => {
+    pressed.addEventListener('click', newBtn)
 })
